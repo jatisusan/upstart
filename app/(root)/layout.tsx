0cfe6f1139c7afc61f1c930b424cf55f051d5f47
jatisusan <susurrus112@gmail.com>
2025-09-24 12:19:01 +0545

    Setup theming and fonts

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main>
+    <main className="font-work-sans">
       <Navbar />
       {children}
     </main>
