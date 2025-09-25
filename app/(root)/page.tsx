7d8c5f89bef8ad1f7984bab2db354495cc9b8dda
jatisusan <susurrus112@gmail.com>
2025-09-25 16:48:49 +0545

    Fetch data from sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
   searchParams,
@@ -8,38 +10,7 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
 
-  const posts = [
-    {
-      _id: "post_001",
-      _createdAt: new Date(),
-      title: "AI-Powered Health Tracker",
-      description:
-        "A wearable that predicts health issues using AI and real-time monitoring.",
-      image:
-        "https://images.unsplash.com/photo-1531140035644-4ce1e6e1b85f?q=80&w=873&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      views: 152,
-      author: {
-        _id: "user_001",
-        name: 'Klaus'
-      },
-      category: "HealthTech",
-    },
-    {
-      _id: "post_004",
-      _createdAt: new Date(),
-      title: "Smart Farming Drones",
-      description:
-        "AI-driven drones that optimize crop monitoring and pesticide usage.",
-      image:
-        "https://ugm.ac.id/wp-content/uploads/2024/01/Smart-Farming-Bayu-1.jpg",
-      views: 187,
-      author: {
-        _id: "user_004",
-        name: 'John'
-      },
-      category: "AgriTech",
-    },
-  ];
+  const posts = await client.fetch(STARTUPS_QUERY);
 
   return (
     <>
@@ -62,7 +33,9 @@ export default async function Home({
 
         <ul className="mt-7 card_grid">
           {posts.length > 0 ? (
-            posts.map((post) => <StartupCard key={post._id} post={post} />)
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post._id} post={post} />
+            ))
           ) : (
             <p className="no-results">No startups found</p>
           )}
--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -3,17 +3,12 @@ import { EyeIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { Author, Startup } from "@/sanity/types";
+
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
-  const {
-    _createdAt,
-    _id,
-    author: { _id: authorId, name },
-    title,
-    description,
-    image,
-    category,
-  } = post;
+  const { _createdAt, _id, author, title, description, image, category } = post;
   return (
     <li className="startup-card group">
       <div className="flex-between">
@@ -26,14 +21,14 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
 
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${authorId}`}>
-            <p className="text-16-medium line-clamp-1">{name}</p>
+          <Link href={`/user/${author?._id}`}>
+            <p className="text-16-medium line-clamp-1">{author?.name}</p>
           </Link>
           <Link href={`/startup/${_id}`}>
             <h3 className="text-26-semibold line-clamp-1">{title}</h3>
           </Link>
         </div>
-        <Link href={`/user/${authorId}`}>
+        <Link href={`/user/${author?._id}`}>
           <Image
             src="https://placehold.co/48x48"
             alt="avatar"
@@ -47,7 +42,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
       <Link href={`/startup/${_id}`}>
         <p className="startup-card_desc">{description}</p>
         <Image
-          src={image}
+          src={image!}
           alt="image"
           className="startup-card_img"
           width={1000}
@@ -56,7 +51,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
       </Link>
 
       <div className="flex-between gap-3 mt-5">
-        <Link href={`/?query=${category.toLowerCase()}`}>
+        <Link href={`/?query=${category?.toLowerCase()}`}>
           <p className="text-16-medium">{category}</p>
         </Link>
 
